e0177c334669b69e24f3e5818b2f51e98b9555ac
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-02-11 13:25:14 +0100

    minor changes

--- Part 5/OpCode.java
+++ Part 5/OpCode.java
@@ -1,5 +1,5 @@
-public enum OpCode { 
-    ldc, imul, ineg, idiv, iadd, 
+public enum OpCode {
+    ldc, imul, ineg, idiv, iadd,
     isub, istore, ior, iand, iload,
-    if_icmpeq, if_icmple, if_icmplt, if_icmpne, if_icmpge, 
+    if_icmpeq, if_icmple, if_icmplt, if_icmpne, if_icmpge,
     if_icmpgt, ifne, GOto, invokestatic, dup, pop, label }
